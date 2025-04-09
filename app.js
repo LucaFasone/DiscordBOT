47ac3c72622bd5cb7652258896cee2493a5b260e
LucaFasone <luca.fasone15@gmail.com>
2025-04-10 01:06:19 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+});
+
+client.on('voiceStateUpdate', (_, newState) => {
+  if (newState.channel.members.size === 1) {
+    const targetChannel = newState.guild.channels.cache.find(channel =>
+      channel.name === 'join-logs' && channel.isTextBased()
+    );
+    if (!targetChannel) {
+      console.error("Né il canale corrispondente al vocale né 'general' sono stati trovati");
+      return;
+    }
+    const onlineMembers = newState.guild.members.cache.filter(member =>
+      member.presence && member.presence.status === 'online' &&
+      member.user.username !== "SkibdiBOT" &&
+      member.id !== newState.member.id
+    );
+
+    const mentions = onlineMembers.map(member => `<@${member.id}>`).join(' ');
+    if (mentions) {
+      targetChannel.send(`Il canale vocale ${newState.channel.name} è appena stato occupato! ${mentions}`);
+    }
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
