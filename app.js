c700573e1f4b02ff29f16c66b16d01a0043cec20
LucaFasone <luca.fasone15@gmail.com>
2025-04-10 02:26:13 +0200

    fix:fix

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const client = new Client({
     GatewayIntentBits.GuildMessages
   ]
 });
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 
 });
 
-client.on('voiceStateUpdate', (oldState, newState) => {
-  if (newState.channel && (!oldState.channel || oldState.channel.id !== newState.channel.id) && newState.channel.members.size === 1) {
+client.on('voiceStateUpdate', (_, newState) => {
+  if (newState.channel.members.size === 1) {
     const targetChannel = newState.guild.channels.cache.find(channel =>
       channel.name === 'join-logs' && channel.isTextBased()
     );
@@ -23,13 +24,14 @@ client.on('voiceStateUpdate', (oldState, newState) => {
       console.error("Né il canale corrispondente al vocale né 'general' sono stati trovati");
       return;
     }
+
     const onlineMembers = newState.guild.members.cache.filter(member =>
       member.presence && member.presence.status === 'online' &&
       member.user.username !== "SkibdiBOT" &&
       member.id !== newState.member.id
     );
-
     const mentions = onlineMembers.map(member => `<@${member.id}>`).join(' ');
+
     if (mentions) {
       targetChannel.send(`${newState.member.user.displayName} si è collegato al canale: ${newState.channel.name}! ${mentions}`);
     }
